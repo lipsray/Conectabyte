cd2cb6b5055a8267d78e9f72719bb19f3e18e4e0
lipsray <65302320+lipsray@users.noreply.github.com>
2021-09-06 23:16:53 -0300

    API + Nodemon

---
+++ backend/data.js
@@ -0,0 +1,101 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Fone Gamer REDRAGON AUXP9',
+            category:'Headphone',
+            image:'/images/Fone1.jpg',
+            price:99.99,
+            countInStock:1,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'2',
+            name:'Fone Gamer REDRAGON Babalu',
+            category:'Headphone',
+            image:'/images/Fone2.jpg',
+            price:199.99,
+            countInStock:3,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'3',
+            name:'Mouse gamer Exemplo1',
+            category:'Mouse',
+            image:'/images/Mouse1.jpg',
+            price:49.99,
+            countInStock:6,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'4',
+            name:'Mouse Gamer Soubom',
+            category:'Mouse',
+            image:'/images/Mouse2.png',
+            price:599.99,
+            countInStock:0,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'5',
+            name:'Teclado Gamer birimbaugaita',
+            category:'Keyboard',
+            image:'/images/Teclado1.jpg',
+            price:799.99,
+            countInStock:9,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'6',
+            name:'Teclado SUQMA',
+            category:'Keyboard',
+            image:'/images/Teclado2.jpg',
+            price:1099.99,
+            countInStock:1,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'7',
+            name:'Fone Gamer UwU',
+            category:'Keyboard',
+            image:'/images/FoneUWU.jpeg',
+            price:799.99,
+            countInStock:13,
+            brand:'Redragon',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+        {
+            _id:'8',
+            name:'Mouse Gamer Razer UX9322',
+            category:'Mouse',
+            image:'/images/Razer.jpg',
+            price:999.99,
+            countInStock:0,
+            brand:'Razer',
+            rating:5,
+            numReviews: 10,
+            description: 'Produto de alta qualidade'
+        },
+    ],
+};
+export default data;
